f3bcbaca81d839c5150bfa3c1df524272e3568c1
Pragya <pragya142018@gmail.com>
2024-05-14 23:45:26 +0530

    updating css

--- src/components/About/Discover.jsx
+++ src/components/About/Discover.jsx
@@ -7,7 +7,7 @@ const Discover = () => {
     <div className="DicoverLeft">
         <h1>The Prodigious eLearning  <span className='Edu'>Courses</span> for you</h1>
         <p>Sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. Labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
-        <button>Discover New Courses</button>
+        <button className='discoverBtn'>Discover New Courses</button>
 
     </div>
    </div>
